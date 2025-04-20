2398505c6539afe070a5b8c8e2f0242a2ba7e176
leemk09 <dlaudrb09@naver.com>
2025-04-20 18:29:52 +0900

    feat: CategoryEntity

---
+++ src/main/java/com/work/library/entity/CategoryEntity.java
@@ -0,0 +1,21 @@
+package com.work.library.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "categories")
+public class CategoryEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    @Comment("카테고리 이름")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+}
